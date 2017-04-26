a5c8603042087a73c892f519d3218d90b044cef2
Melanie <mlpatt23@gmail.com>
2017-04-25 18:07:37 -0700

    Added in projects and skills sections to front-page.php. Updated images to fix distortions

--- front-page.php
+++ front-page.php
@@ -45,11 +45,197 @@
 		</div> <!-- full-width -->
 	</section> <!-- about -->
 
+	<main id="portfolio"> <!-- ID set for Scrollspy/Fragment IDs-->
+		<section class="projects">
+			<h2>Recent Projects</h2>
+			
+			<div class="full-width">
+			
+				<?php
+					$args = array(
+						'post_type' => 'projects',
+						'posts_per_page' => 3,
+						'order' => 'ASC'
+					);
+				
+					query_posts($args);
+					
+					$firstPosts = array();
+				
+					if ( have_posts() ) : while(have_posts()) : the_post();
+					
+						$firstPosts[] = $post->ID;
+				
+						$image = get_field('image');
+						$size = "medium"; ?>
+				
+			
+						<div class="third-width">
+							<div class="individual-project">
+								<a href="mm-studio/index.html"><?php echo wp_get_attachment_image($image, $size); ?></a>
+					
+								<div class="overlay">
+					
+									<h3><?php the_title(); ?></h3>
+									<div>
+										<a href="mm-studio/index.html" class="description">View Description</a>
+										<a href="" class="site  coming-soon">Visit Site</a>
+										<p>Coming Soon!</p>
+									</div>
+						
+								</div> <!-- overlay -->
+							</div> <!-- individual-project -->
+						</div> <!-- third-width -->
+				
+					<?php endwhile; wp_reset_postdata(); ?>
+				<?php endif; ?>
+				
+			</div> <!-- full-width -->
+			
+			
+			
+			<div class="full-width">
+			
+				<?php
+					$args = array(
+						'post_type' => 'projects',
+						'post__not_in' => $firstPosts,
+						'posts_per_page' => 3,
+						'order' => 'ASC'
+					);
+				
+					query_posts($args);
+					
+					$firstPosts = array();
+				
+					if ( have_posts() ) : while(have_posts()) : the_post();
+					
+						$firstPosts[] = $post->ID;
+				
+						$image = get_field('image');
+						$size = "medium"; ?>
+				
+			
+						<div class="third-width">
+							<div class="individual-project">
+								<a href="mm-studio/index.html"><?php echo wp_get_attachment_image($image, $size); ?></a>
+					
+								<div class="overlay">
+					
+									<h3><?php the_title(); ?></h3>
+									<div>
+										<a href="mm-studio/index.html" class="description">View Description</a>
+										<a href="" class="site">Visit Site</a>
+								
+									</div>
+						
+								</div> <!-- overlay -->
+							</div> <!-- individual-project -->
+						</div> <!-- third-width -->
+				
+					<?php endwhile; wp_reset_postdata(); ?>
+				<?php endif; ?>
+				
+			</div> <!-- full-width -->		
+		</section> <!-- projects -->
+	</main>
+
+
+	<section class="skills">
+		<div class="full-width">
+			<h2>Skills</h2>
+			
+			<div class="half-width">
+				
+				<?php
+					$args = array(
+						'post_type' => 'skills',
+						'posts_per_page' => 2,
+						'order' => 'ASC'
+					);
+				
+					query_posts($args);
+					
+					$firstPosts = array();
+				
+					if ( have_posts() ) : while(have_posts()) : the_post();
+					
+						$firstPosts[] = $post->ID;
+				
+						$image = get_field('image_1');
+						$size = "medium"; ?>
+				
+				
+						<div class="fourth-width">
+							
+							<?php echo wp_get_attachment_image($image, $size); ?>
+							
+							<?php the_content(); ?>
+							
+						</div> <!-- fourth-width -->
+						
+				
+					<?php endwhile; wp_reset_postdata(); ?>
+				<?php endif; ?>
+	
+			</div> <!-- half-width -->
+			
+			
+			
+			<div class="half-width">
+				
+				<?php
+					$args = array(
+						'post_type' => 'skills',
+						'post__not_in' => $firstPosts,
+						'posts_per_page' => 2,
+						'order' => 'ASC'
+					);
+				
+					query_posts($args);
+					
+					$firstPosts = array();
+				
+					if ( have_posts() ) : while(have_posts()) : the_post();
+					
+						$firstPosts[] = $post->ID;
+				
+						$image = get_field('image_1');
+						$size = "medium"; ?>
+				
+						<div class="fourth-width">
+							
+							<?php echo wp_get_attachment_image($image, $size); ?>
+							
+							<?php the_content(); ?>
+					
+						</div> <!-- fourth-width -->
+						
+					<?php endwhile; wp_reset_postdata(); ?>
+				<?php endif; ?>	
+					
+			</div> <!-- half-width -->
+		</div> <!-- full-width -->
+	</section> <!-- skills -->
 
 
 
-
-
+	<section class="contact" id="contact" style="background-image:url(<?php echo get_template_directory_uri(); ?>/img/workspace.jpg)"> <!-- ID set for Scrollspy/Fragment IDs-->
+		<div class="full-width">
+			<div class="third-width">
+				<div class="box" id="contact-box">
+					<h3>Think we should work together?<br>I do too!</h3>
+					
+					<?php while(have_posts()) : the_post(); ?>
+		
+						<p><?php the_content(); ?></p>
+		
+					<?php endwhile; ?>
+					
+				</div> <!-- box -->
+			</div> <!-- third-width -->
+		</div> <!-- full-width -->
+	</section> <!-- contact -->
 	
 
 
